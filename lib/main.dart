53bcdb02fb9087dbddb4eb81fbfc133b26d691ab
duke <webcreation33@yahoo.fr>
2022-02-07 09:20:45 +0100

    AV09 usb

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,12 @@ import 'package:flutter_project4/models/google_maps_model.dart';
 import 'package:provider/provider.dart';
 import 'cubit/auth_cubit.dart';
 import 'screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   UserDatabase userDatabase = UserDatabase();
   userDatabase.initialize();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
